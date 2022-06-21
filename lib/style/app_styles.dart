49b39c49ea4bc2999b23fa3d2713c62d7f12346f
Oleksandr Raspopov <107881921+SashaRasppv@users.noreply.github.com>
2022-06-21 12:27:26 +0200

    Formatted

--- lib/style/app_styles.dart
+++ lib/style/app_styles.dart
@@ -4,15 +4,18 @@ import 'package:google_fonts/google_fonts.dart';
 class AppStyles {
   
   static TextStyle titleStyle = GoogleFonts.righteous(
-      fontSize: 20, color: const Color.fromRGBO(37, 29, 58, 1));
+      fontSize: 20, color: const Color.fromRGBO(37, 29, 58, 1)
+  );
   
   static TextStyle contentStyle = GoogleFonts.readexPro(
       fontSize: 16,
       fontWeight: FontWeight.normal,
-      color: const Color.fromRGBO(37, 29, 58, 1));
+      color: const Color.fromRGBO(37, 29, 58, 1)
+  );
   
   static TextStyle date = GoogleFonts.readexPro(
       fontSize: 12,
       fontWeight: FontWeight.normal,
-      color: const Color.fromRGBO(37, 29, 58, 1));
+      color: const Color.fromRGBO(37, 29, 58, 1)
+  );
 }
